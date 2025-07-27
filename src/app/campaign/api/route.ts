47a075c2ce47004dc1acd4cf81e3d9c7fd9a0cbd
Adriel Arocha <adrielarochaoronoz@gmail.com>
2025-07-27 15:34:39 +0200

    get list of campagins

--- src/app/campaign/api/route.ts
+++ src/app/campaign/api/route.ts
@@ -1,18 +1,13 @@
 import { getCollection } from '@/lib/mongo';
 import { NextRequest, NextResponse } from 'next/server';
 
-// Simulación de campañas (reemplaza esto con tu lógica de base de datos)
-const campaigns = [
-    { id: 1, name: 'Campaña A', status: 'activa' },
-    { id: 2, name: 'Campaña B', status: 'inactiva' },
-];
 
 export async function GET(req: NextRequest) {
-
-
     const collection = await getCollection('campaigns');
     const campaignsList  = await collection.find({}).toArray();
-    
+    if (!campaignsList || campaignsList.length === 0) {
+        return NextResponse.json({ error: 'No campaigns found' }, { status: 404 });
+    }
 
     return NextResponse.json(campaignsList);
 }
