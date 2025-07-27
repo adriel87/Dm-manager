17edfa3d12eb8d3cc459a839b6c14ec61fb65c8d
Adriel Arocha <adrielarochaoronoz@gmail.com>
2025-07-27 16:00:33 +0200

    add get by id

---
+++ src/app/campaign/api/[id]/route.ts
@@ -0,0 +1,15 @@
+import { getCollection } from '@/lib/mongo';
+import { ObjectId } from 'mongodb';
+import { NextRequest, NextResponse } from 'next/server';
+
+
+export async function GET(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+    const collection = await getCollection('campaigns');
+    const id = (await params).id;
+    const campaign = await collection.findOne({ _id: new ObjectId(id) });
+    if (!campaign) {
+        return NextResponse.json({ error: 'Campaign not found' }, { status: 404 });
+    }
+
+    return NextResponse.json(campaign);
+}s
