f5e1529250da49c55333cca30546f2c849103f73
Adriel Arocha <adrielarochaoronoz@gmail.com>
2025-08-05 17:55:38 +0200

    [UPDATE] enhance group repository methods with error handling and type adjustments

--- src/domain/group/groupRepository.ts
+++ src/domain/group/groupRepository.ts
@@ -1,11 +1,12 @@
+import { Character } from "../character/character";
 import { Group } from "./group";
 
 export interface GroupRepository {
     createGroup(group: Omit<Group, 'id' | 'createdAt' | 'updatedAt'>): Promise<Group>
     getGroupById(id: string): Promise<Group | null>
     getAllGroups(): Promise<Group[]>
-    updateGroup(id: string, group: Partial<Group>): Promise<Group | null>
-    addMembersToGroup(groupId: string, memberIds: string[]): Promise<Group | null>
-    removeMembersFromGroup(groupId: string, memberIds: string[]): Promise<Group | null>
+    updateGroup(id: string, group: Group): Promise<Group | null>
+    addMembersToGroup(groupId: string, members: Pick<Character, 'id' | 'name' | 'classType'>[]): Promise<boolean>
+    removeMembersFromGroup(groupId: string, memberIds: string[]): Promise<boolean>
     deleteGroup(id: string): Promise<boolean>
 }
