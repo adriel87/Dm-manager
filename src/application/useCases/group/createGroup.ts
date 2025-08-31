8b714f9c29c42a2e33cdb32aa67eb2730c074fce
cudiousidy <idairaalemanquintana@gmail.com>
2025-08-31 12:53:28 +0100

    [UPDATE]: use cases group

--- src/application/useCases/group/createGroup.ts
+++ src/application/useCases/group/createGroup.ts
@@ -1,12 +1,12 @@
-import { Group } from "@/domain/group/group";
+import { Group, validateGroup } from "@/domain/group/group";
 import { GroupRepository } from "@/domain/group/groupRepository";
 
 export const createGroup = async (groupRepository : GroupRepository, group: Omit<Group, "id" | "createdAt" | "updatedAt">) : Promise<Group> => {
     //Validate the group object
      if (!groupRepository || !('createGroup' in groupRepository))  throw new Error('Invalid Repository');
-
-     if (!group || !group.name?.trim() || !group.description?.trim()) throw new Error('Invalid group data');
-
+     
+     validateGroup(group)
+    
      const groupData : Omit<Group, "id"> = {
         ...group,
         createdAt: new Date(),
--- src/domain/group/groupRepository.ts
+++ src/domain/group/groupRepository.ts
@@ -2,7 +2,7 @@ import { Character } from "../character/character";
 import { Group } from "./group";
 
 export interface GroupRepository {
-    createGroup(group: Omit<Group, 'id' | 'createdAt' | 'updatedAt'>): Promise<Group>
+    createGroup(group: Omit<Group, 'id'>): Promise<Group>
     getGroupById(id: string): Promise<Group | null>
     getAllGroups(): Promise<Group[]>
     updateGroup(id: string, group: Group): Promise<Group | null>
