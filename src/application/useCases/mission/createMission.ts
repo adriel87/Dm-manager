eb41d46ad0e2f879ee588e904a730f3b3e47d3ac
cudiousidy <idairaalemanquintana@gmail.com>
2025-09-19 11:46:15 +0100

    [UPDATE] test and useCases mission

--- src/application/useCases/mission/createMission.ts
+++ src/application/useCases/mission/createMission.ts
@@ -1,12 +1,12 @@
-import { Mission } from "@/domain/mission/mission";
+import { Mission, validateMission } from "@/domain/mission/mission";
 import { MissionRespository } from "@/domain/mission/MissionRepository";
 
-export const createMission = async (repository: MissionRespository, missionData: Omit<Mission, "id">): Promise<Mission> => {
-    try {
-        const newMission = await repository.createMission(missionData);
-        return newMission;
-    } catch (error) {
-        console.error("Error creating mission:", error);
-        throw new Error("Failed to create mission");
-    }
+export const createMission = async (repository: MissionRespository, mission: Omit<Mission, "id">): Promise<Mission> => {
+    
+    validateMission(mission);
+
+    const createMission = await repository.createMission(mission);
+
+    return createMission;
+ 
 }
--- src/application/useCases/mission/getMissionById.ts
+++ src/application/useCases/mission/getMissionById.ts
@@ -2,11 +2,11 @@ import { Mission } from "@/domain/mission/mission";
 import { MissionRespository } from "@/domain/mission/MissionRepository";
 
 export const getMissionById = async (repository: MissionRespository, id: string): Promise<Mission | null> => {
-    try {
-        const mission = await repository.getMissionById(id);
-        return mission;
-    } catch (error) {
-        console.error("Error fetching mission by ID:", error);
-        throw new Error("Failed to fetch mission by ID");
-    }
+
+    if (id === null || id === undefined || id === '') throw new Error('Invalid group id');
+
+    const mission = await repository.getMissionById(id);
+    
+    return mission;
+
 }
